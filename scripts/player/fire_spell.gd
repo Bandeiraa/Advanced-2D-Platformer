46cd8877a4f1d53a02fda2b760f58b3b4a41923b
Davi <fcodavibandeira@edu.unifor.br>
2022-04-17 13:54:14 -0300

    Adding Explosion Particles on Fire Spell

--- scripts/player/fire_spell.gd
+++ scripts/player/fire_spell.gd
@@ -5,3 +5,7 @@ func _ready() -> void:
 	for children in get_children():
 		if children is Particles2D:
 			children.emitting = true
+			
+			
+func on_animation_finished(_anim_name: String) -> void:
+	queue_free()
